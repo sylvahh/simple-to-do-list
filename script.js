31c48c988cfb33c88f3c79c54cb181e35ef63fd6
Rozzae <uchechukwueze70@gmail.com>
2022-01-17 12:35:17 +0100

    Added event listener, function & button to clear completed

--- script.js
+++ script.js
@@ -2,57 +2,69 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var listItem = document.getElementsByTagName("li");
+var clearcompleted = document.getElementById('clearcompleted');
 
 function inputLength() {
-  return input.value.length;
+	return input.value.length;
 }
 
 // create list item
 function createListElement() {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
-  delBtn;
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	delBtn;
 
-  // create delete button
-  var btn = document.createElement("button");
-  btn.appendChild(document.createTextNode("delete"));
-  li.appendChild(btn);
-  btn.onclick = removeParent;
+	// create delete button
+	var btn = document.createElement("button");
+	btn.appendChild(document.createTextNode("delete"));
+	li.appendChild(btn);
+	btn.onclick = removeParent;
 }
 
 // click event
 function addListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  }
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
 //keypress event
 function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
-    createListElement();
-  }
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
 
 // stikethrough event
 function crossOffItem(event) {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("done");
-  }
+	if (event.target.tagName === "LI") {
+		event.target.classList.toggle("done");
+	}
+}
+
+// Function to clear all completed items
+function clearComleted(){
+	var listItems = [...document.getElementsByClassName("done")];
+	listItems.forEach(element => {
+		element.parentNode.removeChild(element);
+	});
 }
 
 function listLength() {
-  return listItem.length;
+	return listItem.length;
 }
 
 // delete function
 function removeParent(evt) {
-  evt.target.parentNode.remove();
+	evt.target.parentNode.remove();
 }
 
 // event listeners
 ul.addEventListener("dblclick", crossOffItem);
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
+
+// Event listener to clear completed
+clearcompleted.addEventListener('click', clearComleted);
